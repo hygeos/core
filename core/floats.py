462de7ebb486e87991be38829632ca8d45bad7a6
Joackim Orcière <joackim.orciere@hygeos.com>
2024-08-09 15:39:19 +0200

    feat: added 'floats' module for fp arithmetics
        Function core.floats.equal to compare floating points

---
+++ core/floats.py
@@ -0,0 +1,21 @@
+# standard library imports
+import math
+        
+# third party imports
+# ...
+        
+# sub package imports
+# ...
+
+def equal(a: float, b: float, *, tol=1e-6):
+    """
+    returns true if a and b are within tol of difference
+    """
+    
+    # works with infinite
+    if (a == b): return True
+    
+    # compute diff
+    d = math.fabs(a - b)
+    
+    return d < tol
