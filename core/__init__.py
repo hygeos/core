6b311f7a52ed63a37118d28e63a2fee8267500c1
François Steinmetz <fs@hygeos.com>
2024-08-22 11:11:26 +0200

    arch: add imports in `__init__.py`

--- core/__init__.py
+++ core/__init__.py
@@ -3,4 +3,6 @@ Common Operations for Robust Engineering
 
 A collection of common procedures for generic programming purposes
 """
-from .Chrono import Chrono
+from core.Chrono import Chrono
+from core.fileutils import filegen, mdir
+from core.save import to_netcdf
