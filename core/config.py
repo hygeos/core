1fcdfeb806e8b51a04af09d1bc6b749e766157e0
Joackim Orcière <joackim.orciere@hygeos.com>
2025-01-14 09:21:23 +0100

    feat: Improved config module

--- core/config.py
+++ core/config.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 from typing import Optional, Any
-
+from copy import deepcopy
 import toml
 
+from core.static import constraint
 from core.static import interface
 from core import log
 
@@ -11,52 +12,84 @@ from core import log
 Generic project configuration module.
 Simpler version of core.config module, more generic
 Config path need to be passed as argument
-
-Use example:
-
-cfg = Config(__file__.parent / "project-config.toml")
-cfg.get("key", default=-1)
-
 """
 
-
 class Config:
+    
     @interface
-    def __init__(self, config_path: Path, allow_home_config: bool = False):
-        """ Read and initialize the toml configuration file as a dictionnary0
-
-        Args:
-            config_path (Path): path of the toml config file to load
-            section (str, optional): Only read section as root if provided. Defaults to None.
-
-        Raises:
-            RuntimeError: _description_
-            KeyError: _description_
-        """        
-
-        if not config_path.name.endswith(".toml"):
-            raise RuntimeError(f"Invalid config file extension {config_path}: expected toml")
-
-        if not config_path.is_file():
+    def new_from_toml(config_path: Path, allow_empty_if_no_file: bool = False):
+        
+        file_found = config_path.is_file() and config_path.name.endswith(".toml")
+        
+        if not file_found and not allow_empty_if_no_file:
             raise RuntimeError(f"Invalid config file path {config_path}")
         
-        if allow_home_config is True:
+        configuration = {} if not file_found else toml.load(config_path)
+        
+        return Config(configuration)
+    
+    @interface
+    def new_from_dict(config_dict: dict):
+        return Config(config_dict)
+    
+    @interface
+    def copy(self):
+        return Config(deepcopy(self.config_dict))
+    
+    @interface
+    def __init__(self, config_dict: dict):  
+        self.config_dict: dict = config_dict
+    
+    @interface
+    def get_subsection(self, subsection: str=None, default:dict=None) -> dict:
+        """
+        Returns the specified toml subsection
+        returns the root if None provided
+        """
+        config_ptr = self.config_dict # get config content
+        
+        # Section or nested subsection
+        if subsection is not None: 
+            sections = subsection.split('.')
             
-            base_config_path = Path.home() / ".config/Hygeos"/ config_path.name
-            if base_config_path.is_file():
-                
-                self.cfg_dict = Config.load_toml_as_dict(base_config_path)
-                self.cfg_dict.update(Config.load_toml_as_dict(config_path))
-                log.debug(f"Overriding home config {base_config_path} with {config_path}")
+            # find subsection
+            for sub in sections:
+                if sub in config_ptr:
+                    config_ptr = config_ptr[sub]
+                else:
+                    if default is not None:
+                        return default
+                    raise KeyError(f"Missing section [{subsection}] in config file")
+        return config_ptr
+
+
+    @interface
+    def ingest(self, config_dict, override=True):
+        """
+        Ingest a configuration, if override is true then doubled keys are overriden by new config
+        otherwise only add missing keys from new config
+        keeping the keys defined in configuration and not overriden by itself.
+        """
+        
+        if isinstance(config_dict, dict): # already a dict
+            pass
+        elif isinstance(config_dict, Config):
+            config_dict = config_dict.config_dict
         else:
-            self.cfg_dict = Config.load_toml_as_dict(config_path)
-
-
-
+            log.error(f"Unexpected argument type \'{type(config_dict)}\', expected dict or Config object", e=ValueError)  
+            
+        for k, v in config_dict.items():  # iterate provided defaults keys, vals
+            if k in self.config_dict:      # if the key is not already defined
+                if not override:
+                    continue
+            self.config_dict[k] = v        
+            
+        
+    @interface
     def get(
         self,
         key: str,
-        section: str = None,
+        subsection: str = None,
         *,
         default=None,
     ) -> Any:
@@ -68,44 +101,24 @@ class Config:
         auto cast values to Path objects if its key starts with 'dir_' and its type is str
         """
 
-        # Base section
-        if section is None:
-            current_config = self.cfg_dict # get config content
-        
-        # Section or nested subsection
-        else: 
-            sections = section.split('.')
-            current_config = self.cfg_dict
-            
-            # find subsection
-            for sub in sections:
-                if sub in current_config:
-                    current_config = current_config[sub]
-                else:
-                    raise KeyError(f"Missing section [{section}] in config file")
+        config_dict_ptr = self.get_subsection(subsection=subsection)
             
         # key managing
-        if key not in current_config:
+        if key not in config_dict_ptr:
             if default is not None:
                 return default
             else:
-                mess = "root section" if section is None else f"section [{section}]"
-                raise KeyError( f"Missing key {key} in {mess} of in config file")
+                mess = "root section" if subsection is None else f"section [{subsection}]"
+                raise KeyError( f"Missing key \'{key}\' in {mess} of in config file")
                 
         # get value
-        value = current_config[key]
+        value = config_dict_ptr[key]
         
         # auto convert value to path if its key starts with 'dir_'
         if type(value) is str and key.startswith("dir_"):
             value = Path(value)
 
-        return value 
+        return value
     
-    @interface
-    def load_toml_as_dict(toml_path: Path):
-        
-        # read config
-        assert toml_path.is_file()
-        dictio = toml.load(toml_path)
-        
-        return dictio
+    def __str__(self):
+        return str(self.config_dict)
