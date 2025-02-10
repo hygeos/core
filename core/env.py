3631726af608b7bf2d5f7d71243b2989df7e1ec6
Joackim Orcière <joackim.orciere@hygeos.com>
2025-02-10 16:58:27 +0100

    Feat: core.env added generic getvar function

--- core/env.py
+++ core/env.py
@@ -7,6 +7,35 @@ from core import log
 load_dotenv(dotenv_path=find_dotenv(usecwd=True)) 
 
 
+def getvar(
+    envvar: str,
+    default = None,
+    ):
+    """
+    Returns the value of environment variable `envvar`. If this variable is not defined, returns default.
+
+    The environment variable can be defined in the users `.bashrc`, or in a file `.env`
+    in the current working directory.
+
+    Args:
+        envvar: the input environment variable
+        default: the default return, if the environment variable is not defined
+    
+    Returns:
+        the requested environment variable or the default if the var is not defined and a default has been provided.
+    """
+    variable = None
+    use_default = envvar not in environ
+    if envvar in environ:
+        variable = environ[envvar]
+        
+    elif default is None:
+        raise KeyError(f"{envvar} is not defined, and no default has been provided.")
+    else:
+        variable = default
+
+    return variable
+
 def getdir(
     envvar: str,
     default: Optional[Path] = None,
