2e6986a6dec9cdcfe814915e76673c5a8040509b
Nathan Capon <nathan.capon@hygeos.com>
2025-05-04 17:39:51 +0200

    Feat: Add download nextcloud function

--- core/download.py
+++ core/download.py
@@ -1,9 +1,13 @@
 import subprocess
+from os import system
 from pathlib import Path
 from typing import Literal
 
-from core.fileutils import filegen
+from core.fileutils import filegen, mdir
+from core.uncompress import uncompress
 from core import log
+
+sharelink_eoread = 'https://docs.hygeos.com/s/Fy2bYLpaxGncgPM/'
 
 def download_url(
     url: str,
@@ -42,3 +46,34 @@ def download_url(
     download_target(target)
 
     return target
+
+def download_nextcloud(product_name: str, 
+                       output_dir: Path | str, 
+                       input_dir: Path | str = ''):
+    """
+    Function for downloading data from Nextcloud contained in the data/eoread directory
+
+    Args:
+        product_name (str): Name of the product with the extension
+        output_dir (Path | str): Directory where to store downloaded data
+        input_dir (Path | str, optional): Sub repository in which the product are stored. Defaults to ''.
+
+    Returns:
+        Path: Output path of the downloaded data
+    """
+    
+    @filegen(arg=0)
+    def download_with_filegen(output_path, input_path):
+        system(f'wget {sharelink_eoread}/download?files={input_path} -c -O {output_path}')
+    
+    output_dir = mdir(output_dir)
+    outpath    = output_dir/product_name
+    inputpath  = Path(input_dir)/product_name
+    
+    download_with_filegen(outpath, inputpath)
+    
+    # Uncompress downloaded file 
+    if product_name.split('.')[-1] in ['zip','gz']:
+        return uncompress(outpath, output_dir)
+        
+    return outpath
