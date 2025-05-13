ff7ecec52a443e3f077b2f0ce76b4d0627063629
Joackim Orcière <joackim.orciere@hygeos.com>
2025-05-13 17:50:32 +0200

    Feat: core.log improvements config prefix, prompt, disp, propagates kwargs

--- core/log.py
+++ core/log.py
@@ -9,6 +9,7 @@
 # standard library imports
 from datetime import datetime
 from enum import Enum
+from typing import Literal
 from tqdm import tqdm
 import inspect
 import warnings
@@ -19,15 +20,13 @@ if 'ipykernel' in sys.modules: from tqdm.notebook import tqdm
 else: from tqdm import tqdm
         
 # sub package imports
-# ...
+from core import env
+from string import Template
+import os
 
 
 class config:
-    show_level = True
-    show_namespace = False
-    show_time = True
     show_color = True
-
 
 class _color:
     default = '\033[0m'
@@ -55,6 +54,7 @@ class rgb:
     bold        = _color('\033[1m')
     underline   = _color('\033[4m')
     default     = _color(_color.default)
+    gray        = _color('\033[90m')
     
 # set levels enums
 class lvl(Enum):
@@ -62,38 +62,64 @@ class lvl(Enum):
     INFO = 2 
     WARNING = 3
     ERROR = 4
-    CRITICAL = 5
+    PROMPT = 5
 
 # set levels colors
 lvl.DEBUG.color     = rgb.purple
 lvl.INFO.color      = rgb.blue
 lvl.WARNING.color   = rgb.orange
 lvl.ERROR.color     = rgb.red
-lvl.CRITICAL.color  = rgb.red
-    
+lvl.PROMPT.color    = rgb.cyan
+
+lvl.DEBUG.icon     = "(d)"
+lvl.INFO.icon      = "(i)"
+lvl.WARNING.icon   = "/!\\"
+lvl.ERROR.icon     = "/x\\"
+lvl.PROMPT.icon    = "(?)"
+
+# "/!\\"
+# "/!\\"
+
 class _internal:
     min_global_level = lvl.DEBUG
     blacklist = {}
     
+    prefix = env.getvar("HYGEOS_LOG_PREFIX_FORMAT", default="$level $time")
+    
+    
     def format_msg(level: lvl, msg: str, mod):
         
-        lvl_prefix = "" # construct prefixes depending on options
-        namespace_prefix = ""
-        time_prefix = ""
+        prefix = _internal.prefix
+                
+        kwargs = {}
+        if "$level" in prefix: # status, level
+            kwargs["level"] = f"{level.color}" + f"[{level.name.lower()}]".ljust(8+2)
+            
+        if "$namespace" in prefix: # namespace
+            mod_name = "main" if not hasattr(mod, "__name__") else mod.__name__ # because if calling from main mod is None
+            kwargs["namespace"] = f"{rgb.orange}" + f"{mod_name}"
         
-        mod_name = "main" if not hasattr(mod, "__name__") else mod.__name__ # if calling from main mod is None
+        if "$icon" in prefix: # icon
+            kwargs["icon"] = f"{level.color}" + f"{level.icon}"
+            
+        if "$time" in prefix: # time
+            kwargs["time"] = f"{rgb.green}" +f"{datetime.now().strftime('%H:%M:%S')}"
         
-        if config.show_level:       lvl_prefix          += f"[{level.name}] ".ljust(8+2)
-        if config.show_namespace:   namespace_prefix    += f"({mod_name}) "
-        if config.show_time:        time_prefix         += f"{datetime.now().strftime('%H:%M:%S')} "
+        if "$pid" in prefix:
+            kwargs["pid"] = f"{rgb.orange}" + f"{os.getpid()}"
         
-        string = f"{level.color}{lvl_prefix}{rgb.orange}{namespace_prefix}{rgb.green}{time_prefix}{rgb.default}{msg}"
+        prefix = prefix.format(**kwargs) # add sapce if no whitespace at right hand
+        if len(prefix) > 0 and not prefix[-1].isspace():
+            prefix += " "
+        
+        prefix = Template(prefix).substitute(**kwargs)
+        string = f"{prefix}{rgb.default}{msg}"
         
         return string
     
         
         # proxy
-    def log(lvl: lvl, *args):
+    def log(lvl: lvl, *args, **kwargs):
         """
         log with selected lvl
         """
@@ -112,7 +138,7 @@ class _internal:
             if blname in mod.__name__ and lvl.value <= _internal.blacklist[blmod].value:
                 return 
                 
-        print(_internal.format_msg(lvl, msg, mod), file=sys.stderr)
+        print(_internal.format_msg(lvl, msg, mod), file=sys.stderr, **kwargs)
     
     def _loading_bar(**kwargs):
         frame  = inspect.currentframe().f_back.f_back
@@ -156,48 +182,43 @@ def pbar(lvl: lvl = lvl.INFO, iterable=None, desc=None, total=None, leave=True,
     return _internal._loading_bar(**locals())
 
 
-def silence(module, level_and_below : lvl = lvl.CRITICAL):  # blacklist only below certain level ? e.g Log.silence("core.filegen", bellow_level=lvl.ERROR)
-        _internal.blacklist[module] = level_and_below
+def silence(module, lvl_and_below : lvl = lvl.ERROR):  # blacklist only below certain level ? e.g Log.silence("core.filegen", bellow_level=lvl.ERROR)
+    if lvl_and_below == lvl.PROMPT:
+        error("Cannot silence prompts, max level authorized: lvl.ERROR", e=ValueError)
+        
+    _internal.blacklist[module] = lvl_and_below
 
 
-def log(lvl: lvl, *args):
+def disp(*args, **kwargs):
+    msg = _internal.concat_mess(*args)
+    print(msg, file=sys.stderr, **kwargs)
+
+def log(lvl: lvl, *args, **kwargs):
     """
     log with specified level
     """
     
-    _internal.log(lvl, *args)
+    _internal.log(lvl, *args, **kwargs)
 
-def critical(*args, e: Exception=RuntimeError):
-    """
-    log with defaul level CRITICAL
-    will raise e if passed
-    """
     
-    _internal.log(lvl.CRITICAL, *args)
-    
-    if e is not None:
-        if not issubclass(e, Exception):
-            raise RuntimeError(f"log.error Invalid Exception type: {str(e)}, should be a subclass of {str(Exception)}")
-        raise e(*args)
-    
-def error(*args, e: Exception=RuntimeError):
+def error(*args, e: Exception=RuntimeError, **kwargs):
     """
     log with defaul level ERROR
     will raise e if passed
     """
 
-    _internal.log(lvl.ERROR, *args)
+    _internal.log(lvl.ERROR, rgb.red, *args, **kwargs)
     
     if e is not None:
         if not issubclass(e, Exception):
             raise RuntimeError(f"log.error Invalid Exception type: {str(e)}, should be a subclass of {str(Exception)}")
         raise e(*args)
     
-def warning(*args, w: Warning=None):
+def warning(*args, w: Warning=None, **kwargs):
     """
     log with defaul level WARNING
     """
-    _internal.log(lvl.WARNING, *args)
+    _internal.log(lvl.WARNING, rgb.orange, *args, **kwargs)
     
     if w is not None:
         if not issubclass(w, Warning):
@@ -206,11 +227,11 @@ def warning(*args, w: Warning=None):
         msg = _internal.concat_mess(*args)
         warnings.warn(msg, category=w)
     
-def info(*args):
+def info(*args, **kwargs):
     """
     log with default level INFO
     """
-    _internal.log(lvl.INFO, *args)
+    _internal.log(lvl.INFO, *args, **kwargs)
 
 def check(condition, *args, e: Exception=AssertionError):
     """
@@ -218,11 +239,23 @@ def check(condition, *args, e: Exception=AssertionError):
     """
     if not condition: error(*args, e=e)
     
-def debug(*args):
+def debug(*args, **kwargs):
     """
     log with defaul level DEBUG
     """
-    _internal.log(lvl.DEBUG, *args)
+    _internal.log(lvl.DEBUG, *args, **kwargs)
+
+def prompt(*args, **kwargs):
+    """
+    prompt user with log format
+    """
+    _internal.log(lvl.PROMPT, *args, **kwargs)
+
+    return input()
 
 
-
+def set_format(fmt: Literal["$level", "$icon", "$time", "$namespace", "$pid"]):
+    """
+    valid keys: 
+    """
+    _internal.prefix = fmt
