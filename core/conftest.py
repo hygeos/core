088dfa0396d1018642b4d6f1fe258de36c6b0cfb
François Steinmetz <fs@hygeos.com>
2025-09-25 15:23:44 +0200

    Backward compatibility

---
+++ core/conftest.py
@@ -0,0 +1,4 @@
+import warnings
+from core.tests.conftest import *
+
+warnings.warn("Please import from core.tests.conftest", DeprecationWarning)
---
+++ core/pytest_utils.py
@@ -0,0 +1,5 @@
+
+import warnings
+from core.tests.pytest_utils import parametrize_dict
+
+warnings.warn("Please import from core.tests.pytest_utils", DeprecationWarning)
