a0ebbb75e4d536be19ff31ba6eab09a881fdbf71
François Steinmetz <fs@hygeos.com>
2026-01-06 16:19:58 +0100

    Review the implementation of the uncompress module
    - implementation of uncompress_single, designed to uncompress single-file
      archives, which simplifies the workflow
    - this avoids a different behaviour when the archive has a single file
    - uncompress now relies on shutil.unpack_archive
    - uncompress now has an option to uncompress in a directory determined
      from the archive file name, which makes it more deterministic
    - uncompress_decorator does not use a mapping file anymore. A mapping
      function can be provided instead
    - Add tests for the various uncompress functions
    - The uncompress 'bypass' option is now removed.

--- core/files/uncompress.py
+++ core/files/uncompress.py
@@ -5,20 +5,17 @@
 import bz2
 import gzip
 import shutil
-import tarfile
 import subprocess
 import json
 import getpass
 
 from pathlib import Path
 from functools import wraps
-from zipfile import ZipFile
 from datetime import datetime
 from tempfile import TemporaryDirectory, gettempdir, mkdtemp
 from core.dates import duration, now_isofmt
 
 from core import log
-from core.files.lock import LockFile
 
 class ErrorUncompressed(Exception):
     """
@@ -26,52 +23,107 @@ class ErrorUncompressed(Exception):
     """
 
 
-def uncompress_decorator(filename='.core_uncompress_mapping',
-                         verbose=True):
+def uncompress_decorator(target_name_func, verbose=True):
     """
     A decorator that uncompresses the result of function `f`
     
     Signature of `f` is assumed to be as follows:
-        f(identifier, dirname, *args, **kwargs)
+        f(identifier: str, dirname: Path, *args, **kwargs)
     
     The file returned by `f` is uncompressed to dirname
     
-    The mapping of "identifier -> uncompressed" is stored in dirname/filename
+    Parameters:
+    -----------
+    target_name_func : callable
+        A function that takes an identifier and returns the target name/path
+        for the uncompressed file. The identifier can be a URL or any string.
+    verbose : bool
+        Whether to display verbose output during uncompression
     """
-    def read_mapping(mapping_file: Path):
-        if mapping_file.exists():
-            with open(mapping_file) as fp:
-                return json.load(fp)
-        else:
-            return {}
     def decorator(f):
         @wraps(f)
         def wrapper(identifier, dirname, *args, **kwargs):
-            mapping_file = Path(dirname)/filename
-            mapping = read_mapping(mapping_file)
+            # Get target name from the provided function
+            target_name = target_name_func(identifier)
+            target = Path(dirname) / target_name
             
-            if identifier not in mapping:
-                with TemporaryDirectory() as tmpdir:
-                    f_compressed = f(identifier, tmpdir, *args, **kwargs)
-                    target = uncompress(f_compressed, dirname, verbose=verbose)
-
-                    with LockFile(mapping_file):
-                        mapping = read_mapping(mapping_file)
-                        mapping[identifier] = target.name
-                        with open(mapping_file, 'w') as fp:
-                            json.dump(mapping, fp, indent=4)
-            else:
-                target = Path(dirname)/mapping[identifier]
-            assert target.exists()
+            # Check if target already exists
+            if target.exists():
+                return target
+            
+            # Uncompress to target location
+            with TemporaryDirectory() as tmpdir:
+                f_compressed = f(identifier, tmpdir, *args, **kwargs)
+                uncompress(f_compressed, target, verbose=verbose, extract_to_subdir=False)
+                
+                # Find the uncompressed result in dirname and rename if needed
+                uncompressed_files = [p for p in Path(dirname).iterdir() 
+                                    if p.is_file() or p.is_dir()]
+                
+                # Find the most recently created file/directory
+                if uncompressed_files:
+                    newest = max(uncompressed_files, key=lambda p: p.stat().st_mtime)
+                    if newest.name != target_name:
+                        newest.rename(target)
+                
+            assert target.exists(), f"Target {target} does not exist after uncompression"
             return target
         return wrapper
     return decorator
 
 
+def uncompress_single(filename, output_path, verbose=False):
+    """
+    Decompress a single compressed file (not an archive).
+    
+    Parameters:
+    -----------
+    filename : Path
+        Path to the compressed file
+    output_path : Path  
+        Path where the decompressed file should be written
+    verbose : bool
+        Whether to display verbose output
+        
+    Returns:
+    --------
+    Path
+        Path to the decompressed file
+    """
+    fname = str(filename)
+    
+    if fname.endswith('.gz') and not any(fname.endswith(x) for x in ['.tar.gz', '.tgz']):
+        # Single gzip file
+        with gzip.open(fname, 'rb') as f_in, open(output_path, 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
+            
+    elif fname.endswith('.bz2') and not fname.endswith('.tar.bz2'):
+        # Single bzip2 file  
+        with bz2.BZ2File(fname) as f_in, open(output_path, 'wb') as f_out:
+            data = f_in.read()
+            f_out.write(data)
+            
+    elif fname.endswith('.Z'):
+        # Unix compress format
+        cmd = f'gunzip {fname}'
+        if verbose:
+            log.info('Executing:', cmd)
+        if subprocess.call(cmd.split()):
+            raise Exception(f'Error executing command {cmd}')
+        # For .Z files, gunzip creates the output file directly
+        expected_output = filename.parent / filename.stem
+        if expected_output != output_path:
+            shutil.move(expected_output, output_path)
+    else:
+        raise ValueError(f"Unsupported single file compression format: {filename.suffix}")
+    
+    return output_path
+
+
 def uncompress(filename,
                dirname,
-               on_uncompressed='error',
                create_out_dir=True,
+               extract_to_subdir=True,
                verbose=False) -> Path:
     """
     Uncompress `filename` to `dirname`
@@ -79,13 +131,11 @@ def uncompress(filename,
     Arguments:
     ----------
 
-    on_uncompressed: str
-        determines what to do if `filename` is not compressed
-        - 'error': raise an error (default)
-        - 'copy': copy uncompressed file
-        - 'bypass': returns the input file
     create_out_dir: bool
         create output directory if it does not exist
+    extract_to_subdir: bool
+        if True (default), extract to a subdirectory named after the archive
+        if False, extract directly to dirname
 
     Returns the path to the uncompressed file
     """
@@ -99,68 +149,53 @@ def uncompress(filename,
             raise IOError(f'Directory {dirname} does not exist.')
 
     fname = str(filename)
-    with TemporaryDirectory(prefix='tmp_uncompress',
-                            dir=dirname) as tmpdir:
-
-        # uncompress to temporary directory
-        target_tmp = None
-        if fname.endswith('.zip'):
-            with ZipFile(fname) as zipf:
-                zipf.extractall(tmpdir)
-
-        elif True in [fname.endswith(x)
-                      for x in ['.tar.gz', '.tgz', '.tar.bz2', '.tar']]:
-            with tarfile.open(fname) as tarf:
-                tarf.extractall(path=tmpdir)
-        elif fname.endswith('.gz'):
-            target_tmp = Path(tmpdir)/filename.stem
-            with gzip.open(fname, 'rb') as f_in, open(target_tmp, 'wb') as f_out:
-                shutil.copyfileobj(f_in, f_out)
-        elif fname.endswith('.bz2'):
-            target_tmp = Path(tmpdir)/filename.stem
-            with bz2.BZ2File(fname) as f_in, open(target_tmp, 'wb') as f_out:
-                data = f_in.read()
-                f_out.write(data)
-        elif fname.endswith('.Z'):
-            cmd = f'gunzip {fname}'
-            if verbose:
-                log.info('Executing:', cmd)
-            if subprocess.call(cmd.split()):
-                raise Exception(f'Error executing command {cmd}')
-            target_tmp = filename.parent/filename.stem
-            assert target_tmp.exists()
+    
+    with TemporaryDirectory(prefix='tmp_uncompress', dir=dirname) as tmpdir:
+        extracted_path = None
+        
+        # Check if it's a single compressed file first
+        fname = str(filename)
+        if ((fname.endswith('.gz') and not any(fname.endswith(x) for x in ['.tar.gz', '.tgz'])) or
+            (fname.endswith('.bz2') and not fname.endswith('.tar.bz2')) or
+            fname.endswith('.Z')):
+            # Single compressed file
+            extracted_path = Path(tmpdir) / filename.stem
+            uncompress_single(filename, extracted_path, verbose)
         else:
-            if on_uncompressed == 'error':
+            # Try archive formats with shutil.unpack_archive
+            try:
+                shutil.unpack_archive(fname, tmpdir)
+                extracted_path = Path(tmpdir)
+            except (shutil.ReadError, ValueError):
+                # Not a recognized archive format
                 raise ErrorUncompressed(
                     'Could not determine format of file '
                     f'{Path(filename).name} and `allow_uncompressed` is not set.')
-            elif on_uncompressed == 'copy':
-                target_tmp = Path(filename)
-            elif on_uncompressed == 'bypass':
-                return filename
+        
+        # Determine final target path
+        if extract_to_subdir:
+            # Handle compound extensions properly
+            name = filename.name
+            if name.endswith(('.tar.gz', '.tar.bz2', '.tar.xz')):
+                # For compound extensions, remove both parts
+                target_name = Path(filename.stem).stem
             else:
-                raise ValueError(f'Invalid value "{on_uncompressed}" for argument `on_uncompressed`')
-
-        # determine path to uncompressed temporary directory and target
-        target = None
-        if target_tmp is None:
-            lst = list(Path(tmpdir).glob('*'))
-            if len(lst) == 1:
-                target_tmp = lst[0]
-            else:
-                target_tmp = Path(tmpdir)
-                target = Path(dirname)/filename.stem
-
-        # determine target
-        if target is None:
-            target = Path(dirname)/target_tmp.name
-        assert not target.exists(), f'Error, {target} exists.'
-
-        # move temporary to destination
-        shutil.move(target_tmp, target)
-
+                target_name = filename.stem
+            
+            target = Path(dirname) / target_name
+            assert not target.exists(), f'Error, {target} exists.'
+            # Move to final destination
+            shutil.move(extracted_path, target)
+        else:
+            target = Path(dirname)
+            # Move the contents of extracted_path to dirname
+            items = [extracted_path] if extracted_path.is_file() else list(extracted_path.iterdir())
+            for item in items:
+                dest = target / item.name
+                assert not dest.exists(), f'Error, {dest} exists.'
+                shutil.move(item, dest)
+        
     assert target.exists()
-
     return target
 
 
--- tests/files/test_uncompress.py
+++ tests/files/test_uncompress.py
@@ -3,11 +3,16 @@
 
 
 import gzip
+import bz2
+import tarfile
+import io
 from datetime import timedelta
 from pathlib import Path
 from tempfile import NamedTemporaryFile, TemporaryDirectory
+from zipfile import ZipFile
 
-from core.files.uncompress import CacheDir, duration
+from core.files.uncompress import CacheDir, duration, uncompress_decorator, uncompress, uncompress_single, ErrorUncompressed
+import pytest
 
 
 def test_uncompress_cache():
@@ -43,3 +48,154 @@ def test_duration():
     assert duration('2w') == timedelta(weeks=2)
     assert duration('2d') == timedelta(days=2)
     assert duration('2h') == timedelta(hours=2)
+
+
+def test_uncompress_decorator():
+    """Test the uncompress_decorator with a custom target name function"""
+    
+    def url_to_filename(url):
+        """Extract filename from URL, removing extension for uncompressed target"""
+        filename = Path(url).name
+        # Remove compression extensions to get the target name
+        if filename.endswith('.zip'):
+            return filename[:-4]
+        return filename
+    
+    @uncompress_decorator(target_name_func=url_to_filename, verbose=False)
+    def mock_download(identifier, tmpdir):
+        """Mock function that simulates downloading a compressed file"""
+        # Create a sample zip file in tmpdir
+        sample_content = b"This is sample content from a compressed file."
+        zip_path = Path(tmpdir) / f"downloaded_{Path(identifier).stem}.zip"
+        
+        with ZipFile(zip_path, 'w') as zipf:
+            zipf.writestr("sample_data1.txt", sample_content)
+            zipf.writestr("sample_data2.txt", sample_content)
+        
+        return zip_path
+    
+    with TemporaryDirectory(prefix='test_decorator_') as test_dir:
+        # Test with a mock URL identifier
+        test_url = "https://example.com/dataset.zip"
+        
+        # First call should uncompress and create the target
+        result1 = mock_download(test_url, test_dir)
+        expected_target = Path(test_dir) / "dataset"
+        
+        assert result1 == expected_target
+        assert result1.exists()
+        assert result1.is_dir()  # zip extracts to a directory
+        
+        # Check that the content was extracted correctly
+        extracted_file = result1 / "sample_data1.txt"
+        assert extracted_file.exists()
+        assert extracted_file.read_text() == "This is sample content from a compressed file."
+        
+        # Second call should return the same path without re-extracting
+        result2 = mock_download(test_url, test_dir)
+        assert result2 == result1
+        assert result2 == expected_target
+        
+        # Test with a different identifier
+        test_url2 = "https://example.com/other_data.zip"
+        result3 = mock_download(test_url2, test_dir)
+        expected_target2 = Path(test_dir) / "other_data"
+        
+        assert result3 == expected_target2
+        assert result3.exists()
+        assert result3 != result1  # Different targets for different identifiers
+
+
+@pytest.mark.parametrize("format_ext,compress_func", [
+    ('.gz', gzip.open),
+    ('.bz2', bz2.BZ2File),
+])
+def test_uncompress_single_formats(format_ext, compress_func):
+    """Test uncompress_single with different compression formats"""
+    with TemporaryDirectory(prefix='test_single_') as tmpdir:
+        test_content = b"Test content for compression."
+        compressed_file = Path(tmpdir) / f"test.txt{format_ext}"
+        
+        # Create compressed file
+        with compress_func(compressed_file, 'wb') as f:
+            f.write(test_content)
+        
+        # Test decompression
+        output_file = Path(tmpdir) / "decompressed.txt"
+        result = uncompress_single(compressed_file, output_file)
+        
+        assert result == output_file
+        assert output_file.exists()
+        assert output_file.read_bytes() == test_content
+
+
+@pytest.mark.parametrize("archive_format,create_func", [
+    ('zip', lambda path: _create_zip_multi(path)),
+    ('tar.gz', lambda path: _create_tar_gz_multi(path)),
+    ('tar.bz2', lambda path: _create_tar_bz2_multi(path)),
+])
+@pytest.mark.parametrize("extract_to_subdir,expected_files", [
+    (True, lambda tmpdir: [(Path(tmpdir) / "test" / "file1.txt", "content"), 
+                          (Path(tmpdir) / "test" / "subdir" / "file2.txt", "content")]),
+    (False, lambda tmpdir: [(Path(tmpdir) / "file1.txt", "content"), 
+                           (Path(tmpdir) / "subdir" / "file2.txt", "content")]),
+])
+def test_uncompress_archive_formats_and_modes(archive_format, create_func, extract_to_subdir, expected_files):
+    """Test uncompress with different archive formats and extract modes"""
+    with TemporaryDirectory(prefix='test_archive_') as tmpdir:
+        archive_file = Path(tmpdir) / f"test.{archive_format}"
+        
+        # Create archive with multiple files
+        create_func(archive_file)
+        
+        # Test extraction
+        uncompress(archive_file, tmpdir, extract_to_subdir=extract_to_subdir)
+        
+        # Check expected files exist
+        for file_path, content in expected_files(tmpdir):
+            assert file_path.exists()
+            assert file_path.read_text() == content
+
+
+def _create_zip_multi(path):
+    """Helper to create a zip file with multiple files"""
+    with ZipFile(path, 'w') as zf:
+        zf.writestr("file1.txt", "content")
+        zf.writestr("subdir/file2.txt", "content")
+
+
+def _create_tar_gz_multi(path):
+    """Helper to create a tar.gz file with multiple files"""
+    with tarfile.open(path, 'w:gz') as tf:
+        # Add file1.txt
+        info1 = tarfile.TarInfo(name="file1.txt")
+        info1.size = len(b"content")
+        tf.addfile(info1, fileobj=io.BytesIO(b"content"))
+        # Add subdir/file2.txt
+        info2 = tarfile.TarInfo(name="subdir/file2.txt")
+        info2.size = len(b"content")
+        tf.addfile(info2, fileobj=io.BytesIO(b"content"))
+
+
+def _create_tar_bz2_multi(path):
+    """Helper to create a tar.bz2 file with multiple files"""
+    with tarfile.open(path, 'w:bz2') as tf:
+        # Add file1.txt
+        info1 = tarfile.TarInfo(name="file1.txt")
+        info1.size = len(b"content")
+        tf.addfile(info1, fileobj=io.BytesIO(b"content"))
+        # Add subdir/file2.txt
+        info2 = tarfile.TarInfo(name="subdir/file2.txt")
+        info2.size = len(b"content")
+        tf.addfile(info2, fileobj=io.BytesIO(b"content"))
+
+
+def test_uncompress_single_unsupported():
+    """Test uncompress_single with unsupported format"""
+    with TemporaryDirectory(prefix='test_single_') as tmpdir:
+        unsupported_file = Path(tmpdir) / "test.txt"
+        unsupported_file.write_text("plain text")
+        output_file = Path(tmpdir) / "output.txt"
+        
+        with pytest.raises(ValueError, match="Unsupported single file compression format"):
+            uncompress_single(unsupported_file, output_file)
